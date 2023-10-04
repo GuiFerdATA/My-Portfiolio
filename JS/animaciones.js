f008ab3d4849cf80880b7c218c935d3068ba253e
Guille <guille.fer79@hotmail.com>
2023-10-04 14:23:09 -0300

    animaciones js

---
+++ JS/animaciones.js
@@ -0,0 +1,53 @@
+window.sr =ScrollReveal();
+
+
+    sr.reveal('.contenedor-header', {
+        duration: 1600,
+        origin:'top',
+        distance: '100px',
+        reset: true,
+
+    });
+    sr.reveal('.contenido-seccion h2', {
+        duration: 3000,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+
+    });
+    sr.reveal('.contenido-banner', {
+        duration: 2000,
+        origin:'bottom',
+        distance: '100px',
+        reset: true,
+    });
+    sr.reveal('.col', {
+        duration: 2000,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+
+    });
+    sr.reveal('.galeria', {
+        duration: 2000,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+
+    });
+    sr.reveal('.redes', {
+        duration: 2000,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+
+    });
+    sr.reveal('.arriba', {
+        duration: 3000,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+
+    });
+
+    
