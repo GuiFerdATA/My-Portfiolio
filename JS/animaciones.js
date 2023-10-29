9e8ddeef45ccbf16458c9f20033290d05f0ea877
Guille <guille.fer79@hotmail.com>
2023-10-29 16:48:33 -0300

    asd

--- JS/animaciones.js
+++ JS/animaciones.js
@@ -2,50 +2,47 @@ window.sr =ScrollReveal();
 
 
     sr.reveal('.contenedor-header', {
-        duration: 1600,
+        duration: 2600,
         origin:'top',
-        distance: '100px',
-        reset: true,
-
-    });
-    sr.reveal('.contenido-seccion h2', {
-        duration: 3000,
-        origin:'left',
         distance: '100px',
         reset: true,
 
     });
     sr.reveal('.contenido-banner', {
         duration: 2000,
-        origin:'bottom',
-        distance: '100px',
         reset: true,
     });
-    sr.reveal('.col', {
-        duration: 2000,
-        origin:'left',
-        distance: '100px',
+    sr.reveal('h1', {
+        duration: 4000,
+
         reset: true,
 
     });
-    sr.reveal('.galeria', {
+    sr.reveal('.contenido-seccion h2', {
         duration: 2000,
-        origin:'left',
-        distance: '100px',
+        reset: true,
+    });
+    sr.reveal('.contenido-seccion p', {
+        duration: 3900,
+        reset: true,
+    });
+    
+    sr.reveal('.col', {
+        duration: 4000,
+        reset: true,
+    });
+    sr.reveal('.proyecto', {
+        duration: 4000,
         reset: true,
 
     });
     sr.reveal('.redes', {
-        duration: 2000,
-        origin:'left',
-        distance: '100px',
+        duration: 3000,
         reset: true,
 
     });
     sr.reveal('.arriba', {
         duration: 3000,
-        origin:'left',
-        distance: '100px',
         reset: true,
 
     });
